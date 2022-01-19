e0cbe7704d91578fa96539040e892ad31b035346
Antonella García <72472552+alu0101227610@users.noreply.github.com>
2022-01-19 22:49:45 +0000

    fixing test

--- test/repo-rename.js
+++ test/repo-rename.js
@@ -4,9 +4,12 @@ renameRepo = require('../repo-rename')
 
 describe('#Rename', function() {
 it('gets ID from repository', function() {
-renameRepo.getRepoID("ULL-ESIT-DMSI-1920", "prueba-antonella-funciona").should.equal('R_kgDOGbeYPw\n');
+renameRepo.getRepoID("ULL-ESIT-DMSI-1920", "prueba-antonella-funciona1").should.equal('R_kgDOGbeYPw\n');
 });
 it('converts old name into new name', function() {
+  renameRepo.renameRepo("R_kgDOGbeYPw", "prueba-antonella-funciona").should.equal("prueba-antonella-funciona");
+});
+it('restores old name', function() {
   renameRepo.renameRepo("R_kgDOGbeYPw", "prueba-antonella-funciona1").should.equal("prueba-antonella-funciona1");
 });
 });
